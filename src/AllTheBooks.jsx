af59316c37813d7e3e65ce55140b15ea38fded6b
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-09-05 16:53:39 +0200

    fix

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -1,17 +1,17 @@
 import Books from "./data/scifi.json";
 import { Card } from "react-bootstrap";
 
-const AllTheBooks = (props) => (
+const AllTheBooks = () => (
   <div className="container">
     <div className="row">
       {Books.map((book) => (
         <>
-          <div className="col-12 col-md-6 col-lg-4 gy-5">
+          <div className="col-12 col-md-6 col-lg-4 gy-5" key={`book-${book.asin}`}>
             <Card style={{ width: "200px" }} className="m-auto">
               <Card.Img variant="top" src={book.img} />
               <Card.Body>
                 <Card.Title className="fs-6">{book.title}</Card.Title>
-                <Card.Text>{book.category}</Card.Text>
+                <Card.Text>{`${book.price}$`}</Card.Text>
               </Card.Body>
             </Card>
           </div>
