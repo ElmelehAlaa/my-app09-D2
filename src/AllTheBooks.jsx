20eeadc3fb44395db11cb7c32172c7841614d559
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-09-05 17:18:23 +0200

    fix2

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -4,18 +4,16 @@ import { Card } from "react-bootstrap";
 const AllTheBooks = () => (
   <div className="container">
     <div className="row">
-      {Books.map((book) => (
-        <>
-          <div className="col-12 col-md-6 col-lg-4 gy-5" key={`book-${book.asin}`}>
-            <Card style={{ width: "200px" }} className="m-auto">
-              <Card.Img variant="top" src={book.img} />
-              <Card.Body>
-                <Card.Title className="fs-6">{book.title}</Card.Title>
-                <Card.Text>{`${book.price}$`}</Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        </>
+      {Books.map((book, index) => (
+        <div className="col-12 col-md-6 col-lg-4 gy-5" key={`book-${index}`}>
+          <Card style={{ width: "200px" }} className="m-auto">
+            <Card.Img variant="top" src={book.img} />
+            <Card.Body>
+              <Card.Title className="fs-6">{book.title}</Card.Title>
+              <Card.Text>{`${book.price}$`}</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       ))}
     </div>
   </div>
